e03a469f413d7f6a279eeb9aecda2a25359d6a25
Your name <email_address@gmail.com>
2022-08-02 16:07:25 -0500

    .

---
+++ R/nibrs.R
@@ -0,0 +1,233 @@
+source("C:/Users/jkkap/Dropbox/R_project/crimedatatool_helper_nibrs/R/utils.R", echo=TRUE)
+
+offense_files  <- list.files(pattern = "offense.*rds$")
+victim_files   <- list.files(pattern = "victim.*rds$")
+offender_files <- list.files(pattern = "offender.*rds$")
+arrestee_files <- list.files(pattern = "nibrs_arrestee.*rds$")
+arrestee_group_b_files <- list.files(pattern = "group_b.*rds$")
+ucr <- readRDS("D:/ucr_data_storage/clean_data/offenses_known/offenses_known_yearly_1960_2020.rds") %>%
+  select(ORI = ori9,
+         agency = crosswalk_agency_name,
+         year,
+         state,
+         population) %>%
+  filter(year %in% 1991:2020,
+         !is.na(ORI),
+         !is.na(state)) %>%
+  mutate(state  =  capitalize_words(state),
+         agency = capitalize_words(agency))
+ucr_population_only <-
+  ucr %>%
+  select(ORI,
+         year,
+         population)
+ucr_name_only <-
+  ucr %>%
+  select(ORI,
+         agency,
+         state) %>%
+  distinct(ORI, .keep_all = TRUE)
+
+
+#get_agg_data(1991:2020)
+get_agg_data <- function(years) {
+  for (year_temp in years) {
+    offense <- prep_offense(offense_files[grep(year_temp, offense_files)])
+    offense_small <- offense %>%
+      select(ori,
+             unique_incident_id,
+             offense)
+    offense_agg_year <- aggregate_data(offense,
+                                       variables = "gun_involved",
+                                       time_unit = "year")
+    offense_agg_month <- aggregate_data(offense,
+                                        variables = "gun_involved",
+                                        time_unit = "date")
+    rm(offense); gc()
+
+    offender <- prep_offender(offender_files[grep(year_temp, offender_files)],
+                              offense_small)
+    offender_agg_year <- aggregate_data(offender,
+                                        variables = c("sex_of_offender",
+                                                      "race_of_offender",
+                                                      "age_category"),
+                                        time_unit = "year")
+    offender_agg_month <- aggregate_data(offender,
+                                         variables = c("sex_of_offender",
+                                                       "race_of_offender",
+                                                       "age_category"),
+                                         time_unit = "date")
+    rm(offender); gc()
+
+    arrestee <- prep_arrestee(arrestee_files[grep(year_temp, arrestee_files)],
+                              arrestee_group_b_files[grep(year_temp, arrestee_group_b_files)]) %>%
+      filter(ori %in% offense_small$ori)
+    arrestee_agg_year <- aggregate_data(arrestee,
+                                        variables = c("sex_of_arrestee",
+                                                      "race_of_arrestee",
+                                                      "ethnicity_of_arrestee",
+                                                      "age_category",
+                                                      "type_of_arrest"),
+                                        time_unit = "year")
+    arrestee_agg_month <- aggregate_data(arrestee,
+                                         variables = c("sex_of_arrestee",
+                                                       "race_of_arrestee",
+                                                       "ethnicity_of_arrestee",
+                                                       "age_category",
+                                                       "type_of_arrest"),
+                                         time_unit = "date")
+    rm(arrestee); gc()
+
+    victim <- prep_victim(victim_files[grep(year_temp, victim_files)]) %>%
+      filter(ori %in% offense_small$ori)
+    victim_agg_year <- aggregate_data(victim,
+                                      variables = c("sex_of_victim",
+                                                    "race_of_victim",
+                                                    "ethnicity_of_victim",
+                                                    "age_category",
+                                                    "victim_injury"),
+                                      time_unit = "year",
+                                      victim_type = TRUE)
+    victim_agg_month <- aggregate_data(victim,
+                                       variables = c("sex_of_victim",
+                                                     "race_of_victim",
+                                                     "ethnicity_of_victim",
+                                                     "age_category",
+                                                     "victim_injury"),
+                                       time_unit = "date",
+                                       victim_type = TRUE)
+    victim_agg_demographic_totals_year <- get_victim_dem_agg(victim, time_unit = "year")
+    victim_agg_demographic_totals_month <- get_victim_dem_agg(victim, time_unit = "date")
+
+    rm(victim); gc()
+    rm(offense_small); gc()
+
+
+    temp_agg_year <- offense_agg_year %>%
+      left_join(offender_agg_year,                  by = c("ori", "time_unit")) %>%
+      left_join(arrestee_agg_year,                  by = c("ori", "time_unit")) %>%
+      left_join(victim_agg_year,                    by = c("ori", "time_unit")) %>%
+      left_join(victim_agg_demographic_totals_year, by = c("ori", "time_unit"))
+
+
+    saveRDS(temp_agg_year,
+            paste0("C:/Users/jkkap/Dropbox/R_project/crimedatatool_helper_nibrs/data/temp_agg_year_", year_temp, ".rds"))
+
+    temp_agg_month <- offense_agg_month %>%
+      left_join(offender_agg_month,                  by = c("ori", "time_unit")) %>%
+      left_join(arrestee_agg_month,                  by = c("ori", "time_unit")) %>%
+      left_join(victim_agg_month,                    by = c("ori", "time_unit")) %>%
+      left_join(victim_agg_demographic_totals_month, by = c("ori", "time_unit"))
+    saveRDS(temp_agg_month,
+            paste0("C:/Users/jkkap/Dropbox/R_project/crimedatatool_helper_nibrs/data/temp_agg_month_", year_temp, ".rds"))
+
+    message(year_temp)
+  }
+}
+
+combine_agg_data <- function(type) {
+  setwd("C:/Users/jkkap/Dropbox/R_project/crimedatatool_helper_nibrs/data/")
+  files <- list.files(pattern = paste0("temp_agg_", type))
+
+  final <- data.frame()
+  for (file in files) {
+    temp <- readRDS(file)
+    temp[is.na(temp)] <- 0
+    final <- bind_rows(final, temp)
+    rm(temp); gc()
+    message(file)
+  }
+
+  final <- final[, -grep("30a|61a|90k", names(final))]
+  final$ori <- toupper(final$ori)
+  final <- final %>% rename(year = time_unit,
+                            ORI  = ori)
+  return(final)
+}
+
+add_missing_columns <- function(data) {
+  for (col in c("arrestee_age_unknown_sports_tampering",
+                "arrestee_age_unknown_human_trafficking_commercial_sex_acts",
+                "arrestee_age_unknown_human_trafficking_involuntary_servitude",
+                "arrestee_age_unknown_hacking_computer_invasion",
+                "arrestee_age_unknown_animal_cruelty",
+                "arrestee_american_indian_sports_tampering",
+                "arrestee_unknown_race_sports_tampering",
+                "arrestee_american_indian_hacking_computer_invasion",
+                "arrestee_white_sports_tampering",
+                "offender_american_indian_sports_tampering",
+                "victim_adult_animal_cruelty",
+                "victim_age_unknown_animal_cruelty",
+                "victim_hispanic_animal_cruelty",
+                "victim_not_hispanic_animal_cruelty",
+                "victim_asian_animal_cruelty",
+                "victim_american_indian_animal_cruelty",
+                "victim_black_animal_cruelty",
+                "victim_white_animal_cruelty",
+                "victim_female_animal_cruelty")) {
+    if (!all(grepl(col, names(data)))) {
+      data[, col] <- 0
+    }
+  }
+  return(data)
+}
+
+# final_agg_year  <- combine_agg_data(type = "year")
+# final_agg_year  <- add_missing_columns(final_agg_year)
+# final_agg_year  <-
+#   final_agg_year %>%
+#   filter(ORI %in% ucr$ORI) %>%
+#   left_join(ucr_population_only) %>%
+#   left_join(ucr_name_only) %>%
+#   select(ORI,
+#          year,
+#          agency,
+#          state,
+#          population,
+#          everything())
+# setwd("nibrs")
+# make_largest_agency_json(final_agg_year)
+# make_state_agency_choices(final_agg_year)
+# files <- list.files(pattern = "agency_choices")
+# files
+# file.copy(files, paste0(here::here("data/nibrs_monthly/")), overwrite = TRUE)
+# make_agency_csvs(final_agg_year)
+
+final_agg_month <- combine_agg_data(type = "month")
+final_agg_month <- add_missing_columns(final_agg_month)
+final_agg_month$state_abb <- substr(final_agg_month$ORI, 1, 2)
+state_abb <- unique(substr(unique(final_agg_month$ORI), 1, 2))
+state_abb <- sort(state_abb)
+state_abb
+setwd("C:/Users/jkkap/Dropbox/R_project/crimedatatool_helper_nibrs/data/nibrs_monthly")
+for (state_abb_temp in state_abb) {
+  temp <- final_agg_month %>% filter(state_abb %in% state_abb_temp)
+  final_agg_month <- final_agg_month %>% filter(!state_abb %in% state_abb_temp)
+  gc()
+
+  temp$state_abb <- NULL
+  temp  <-
+    temp %>%
+    filter(ORI %in% ucr$ORI) %>%
+    mutate(date = year,
+           year = year(year)) %>%
+    left_join(ucr_population_only, by = c("ORI", "year")) %>%
+    left_join(ucr_name_only, by = "ORI") %>%
+    select(-year) %>%
+    rename(year = date) %>%
+    select(ORI,
+           year,
+           agency,
+           state,
+           population,
+           everything())
+  gc()
+  temp$year <- as.character(temp$year)
+  make_agency_csvs(temp, type = "month")
+}
+
+
+
+
+
+
---
+++ R/utils.R
@@ -0,0 +1,465 @@
+setwd("D:/ucr_data_storage/clean_data/nibrs")
+library(lubridate)
+library(stringr)
+library(fastDummies)
+library(tidyr)
+library(janitor)
+library(dplyr)
+library(splitstackshape)
+library(crimeutils)
+library(here
+        )
+get_victim_dem_agg <- function(data, time_unit) {
+  data <- data %>%
+    filter(type_of_victim %in% c("individual",
+                                 "law enforcement officer")) %>%
+    aggregate_data(time_unit = time_unit)
+  names(data) <- paste0("demographics_total_", names(data))
+  data <-
+    data %>%
+    rename(ori       = demographics_total_ori,
+           time_unit = demographics_total_time_unit)
+  return(data)
+}
+
+
+age_fix <- c("over 98 years old"            = "99",
+             "under 24 hours \\(neonate\\)" = "0",
+             "1-6 days old"                 = "0",
+             "7-364 days old"               = "0")
+
+race_fix <- c("^asian/pacific islander$"                    = "asian",
+              "^P$"                                         = "asian",
+              "^M$"                                         = "unknown",
+              "^native hawaiian or other pacific islander$" = "asian",
+              "^american indian/alaskan native$"            = "american indian")
+ethnicity_fix <- c("^not of hispanic origin$" = "not_hispanic",
+                   "^hispanic origin$"         = "hispanic")
+
+
+# Keep most common year
+keep_most_common_year <- function(data) {
+  most_common_year <- sort(table(data$year), decreasing = TRUE)[1]
+  most_common_year <- names(most_common_year)
+  data <-
+    data %>%
+    filter(year(date) %in% most_common_year)
+  return(data)
+}
+
+
+prep_offense <- function(file) {
+  data <- readRDS(file) %>%
+    mutate(date = ymd(incident_date),
+           date = floor_date(date, unit = "month"),
+           year = year(date)) %>%
+    select(ori,
+           unique_incident_id,
+           date,
+           year,
+           offense = ucr_offense_code,
+           type_weapon_force_involved_1,
+           type_weapon_force_involved_2,
+           type_weapon_force_involved_3) %>%
+    mutate_if(is.character, tolower)
+  data <- keep_most_common_year(data)
+  data$gun_involved <- "no_gun"
+  for (i in 1:3) {
+    data$temp <- data[, paste0("type_weapon_force_involved_", i)]
+    data$temp[is.na(data$temp)] <- "none"
+    data$gun_involved[data$temp %in% c("handgun",
+                                       "firearm (type not stated)",
+                                       "rifle",
+                                       "shotgun",
+                                       "other firearm")] <- "gun"
+    data[, paste0("type_weapon_force_involved_", i)] <- NULL
+  }
+  data$temp <- NULL
+
+  data$offense <- paste("offense", data$offense)
+  data$gun_involved <- paste("offense", data$gun_involved)
+  return(data)
+}
+
+
+prep_victim <- function(file) {
+  data <- readRDS(file) %>%
+    select(ori,
+           type_of_victim,
+           unique_incident_id,
+           ucr_offense_code_1,
+           ucr_offense_code_2,
+           ucr_offense_code_3,
+           ucr_offense_code_4,
+           ucr_offense_code_5,
+           ucr_offense_code_6,
+           ucr_offense_code_7,
+           ucr_offense_code_8,
+           ucr_offense_code_9,
+           ucr_offense_code_10,
+           age_of_victim,
+           sex_of_victim,
+           race_of_victim,
+           ethnicity_of_victim,
+           type_of_injury_1,
+           type_of_injury_2,
+           type_of_injury_3,
+           type_of_injury_4,
+           type_of_injury_5,
+           incident_date) %>%
+    mutate(date = ymd(incident_date),
+           date = floor_date(date, unit = "month"),
+           year = year(date)) %>%
+    select(-incident_date) %>%
+    mutate_if(is.character, tolower)
+  data <- keep_most_common_year(data)
+  data$victim_injury           <- "unknown injury"
+  for (i in 1:5) {
+    data$temp <- data[, paste0("type_of_injury_", i)]
+    data$temp[is.na(data$temp)] <- "unknown injury"
+    data$victim_injury[data$temp %in% "none"] <- "no injury"
+    data$victim_injury[data$temp %in% "apparent minor injuries"] <- "minor injury"
+    data$victim_injury[data$temp %in% c("severe laceration",
+                                        "apparent broken bones",
+                                        "other major injury",
+                                        "unconsciousness",
+                                        "loss of teeth",
+                                        "possible internal injury")] <- "serious injury"
+
+    data[, paste0("type_of_injury_", i)] <- NULL
+  }
+  data$temp <- NULL
+
+  # Loops through each of the 10 UCR offenses and turns people who had multiple
+  # crimes into multiple rows. For example, if someone had 3 crimes then would
+  # becomes 3 rows in the data
+  final <- data.frame()
+  ucr_offense_cols <- grep("ucr_offense", names(data), value = TRUE)
+  for (i in 1:10) {
+    # Keeps all rows where there is a crime - i.e. is not NA
+    temp <- data[!is.na(data[, paste0("ucr_offense_code_", i)]), ]
+    if (nrow(temp) > 0) {
+      temp$offense <- temp[, paste0("ucr_offense_code_", i)]
+      # Drops all the UCR columns
+      temp <- temp[!names(temp) %in% ucr_offense_cols]
+
+      final <- bind_rows(final, temp)
+    }
+  }
+  data <- final
+
+  data$age_of_victim <- str_replace_all(data$age_of_victim, age_fix)
+  data$age_of_victim[data$age_of_victim %in% "unknown"] <- NA
+  data$age_of_victim <- as.numeric(data$age_of_victim)
+
+  data$age_category                                <- "victim_age_unknown"
+  data$age_category[data$age_of_victim %in% 0:17]  <- "victim_juvenile"
+  data$age_category[data$age_of_victim %in% 18:99] <- "victim_adult"
+
+  data$sex_of_victim[data$sex_of_victim %in%
+                       c(NA, "unknown")] <- "unknown_sex"
+  data$race_of_victim[data$race_of_victim %in%
+                        c(NA, "unknown")] <- "unknown_race"
+  data$ethnicity_of_victim[data$ethnicity_of_victim %in%
+                             c(NA, "unknown")] <- "unknown_ethnicity"
+  data$ethnicity_of_victim <- tolower(data$ethnicity_of_victim)
+  data$ethnicity_of_victim <- str_replace_all(data$ethnicity_of_victim, ethnicity_fix)
+
+  data$race_of_victim <- tolower(data$race_of_victim)
+  data$race_of_victim <- str_replace_all(data$race_of_victim, race_fix)
+
+  data$sex_of_victim       <- paste0("victim_", data$sex_of_victim)
+  data$race_of_victim      <- paste0("victim_", data$race_of_victim)
+  data$ethnicity_of_victim <- paste0("victim_", data$ethnicity_of_victim)
+  data$victim_injury       <- paste0("victim_", data$victim_injury)
+  data$offense             <- paste("victim", data$offense)
+  return(data)
+}
+
+
+prep_offender <- function(file, offense_data) {
+  data <- readRDS(file) %>%
+    select(ori,
+           unique_incident_id,
+           age_of_offender,
+           sex_of_offender,
+           race_of_offender,
+           incident_date) %>%
+    mutate(date = ymd(incident_date),
+           date = floor_date(date, unit = "month"),
+           year = year(date)) %>%
+    select(-incident_date) %>%
+    mutate_if(is.character, tolower) %>%
+    filter(ori %in% offense_data$ori) %>%
+    left_join(offense_data, by = c("ori", "unique_incident_id"))
+  data <- keep_most_common_year(data)
+  data$offense <- gsub("^offense ", "", data$offense)
+
+  data$sex_of_offender[data$sex_of_offender %in% c(NA, "unknown")] <- "unknown_sex"
+  data$sex_of_offender <- paste0("offender_", data$sex_of_offender)
+
+  data$race_of_offender <- str_replace_all(data$race_of_offender, race_fix)
+  data$race_of_offender[data$race_of_offender %in% c(NA, "unknown")] <- "unknown_race"
+  data$race_of_offender <- paste0("offender_", data$race_of_offender)
+
+  data$age_of_offender <- str_replace_all(data$age_of_offender, age_fix)
+  data$age_of_offender[data$age_of_offender %in% "unknown"] <- NA
+  data$age_of_offender <- as.numeric(data$age_of_offender)
+  data$age_category <- "offender_age_unknown"
+  data$age_category[data$age_of_offender %in% 0:17]  <- "offender_juvenile"
+  data$age_category[data$age_of_offender %in% 18:99] <- "offender_adult"
+
+
+  data$offense <- paste("offender", data$offense)
+  return(data)
+}
+
+
+prep_arrestee <- function(arrestee_file, arrestee_group_b_file) {
+  arrestee         <- readRDS(arrestee_file)
+  arrestee_group_b <- readRDS(arrestee_group_b_file) %>%
+    select(-arrestee_weapon_1,
+           -arrestee_weapon_2,
+           -automatic_weapon_indicator_1,
+           -automatic_weapon_indicator_2)
+  data         <- bind_rows(arrestee, arrestee_group_b) %>%
+    select(ori,
+           arrest_date,
+           type_of_arrest,
+           offense = ucr_arrest_offense_code,
+           age_of_arrestee,
+           sex_of_arrestee,
+           race_of_arrestee,
+           ethnicity_of_arrestee) %>%
+    mutate(date = ymd(arrest_date),
+           date = floor_date(date, unit = "month"),
+           year = year(date)) %>%
+    select(-arrest_date) %>%
+    mutate_if(is.character, tolower)
+  data <- keep_most_common_year(data)
+
+  data$type_of_arrest <- gsub(" \\(.*", "", data$type_of_arrest)
+
+  data$age_of_arrestee <- str_replace_all(data$age_of_arrestee, age_fix)
+  data$age_of_arrestee[data$age_of_arrestee %in% "unknown"] <- NA
+  data$age_of_arrestee <- as.numeric(data$age_of_arrestee)
+  data$age_category <- "arrestee_age_unknown"
+  data$age_category[data$age_of_arrestee %in% 0:17]  <- "arrestee_juvenile"
+  data$age_category[data$age_of_arrestee %in% 18:99] <- "arrestee_adult"
+
+  data$ethnicity_of_arrestee <- tolower(data$ethnicity_of_arrestee)
+  data$ethnicity_of_arrestee <- str_replace_all(data$ethnicity_of_arrestee, ethnicity_fix)
+
+  data$race_of_arrestee <- str_replace_all(data$race_of_arrestee, race_fix)
+  data$sex_of_arrestee[data$sex_of_arrestee %in% c(NA, "unknown")]             <- "unknown_sex"
+  data$race_of_arrestee[data$race_of_arrestee %in% c(NA, "unknown")]           <- "unknown_race"
+  data$ethnicity_of_arrestee[data$ethnicity_of_arrestee %in% c(NA, "unknown")] <- "unknown_ethnicity"
+
+  data$sex_of_arrestee       <- paste0("arrestee_", data$sex_of_arrestee)
+  data$race_of_arrestee      <- paste0("arrestee_", data$race_of_arrestee)
+  data$ethnicity_of_arrestee <- paste0("arrestee_", data$ethnicity_of_arrestee)
+  data$offense               <- paste("arrestee", data$offense)
+  data$type_of_arrest        <- paste("arrestee", data$type_of_arrest)
+  return(data)
+}
+
+
+aggregate_data <- function(data, variables = NULL, time_unit, victim_type = FALSE) {
+
+  # Drop NIBRS offenses without names - EXTREMELY rare. These are likely very
+  # new subtypes of crimes.
+  if(any(grepl("30A|61A|90K", data$offense))) {
+    data <- data[-grep("30A|61A|90K", data$offense, ignore.case = TRUE), ]
+  }
+
+  weapon_crimes <- c("aggravated assault",
+                     "extortion/blackmail",
+                     "fondling (incident liberties/child molest)",
+                     "human trafficking - commercial sex acts",
+                     "human trafficking - involuntary servitude",
+                     "justifiable homicide",
+                     "kidnapping/abduction",
+                     "murder/nonnegligent manslaughter",
+                     "negligent manslaughter",
+                     "rape",
+                     "robbery",
+                     "sexual assault with an object",
+                     "sodomy",
+                     "weapon law violations")
+  injury_crimes <- c("aggravated assault",
+                     "extortion/blackmail",
+                     "fondling (incident liberties/child molest)",
+                     "human trafficking - commercial sex acts",
+                     "human trafficking - involuntary servitude",
+                     "kidnapping/abduction",
+                     "rape",
+                     "robbery",
+                     "sexual assault with an object",
+                     "simple assault",
+                     "sodomy")
+  person_victim_types <- c("individual",
+                           "law enforcement officer")
+
+  data$time_unit <- data[, time_unit]
+  cols_temp <- c("ori",
+                 "time_unit",
+                 "offense")
+  main_agg <- data %>%
+    count_(cols_temp) %>%
+    pivot_wider(names_from  = offense,
+                values_from = n) %>%
+    rename_all(make_clean_names)
+  main_agg[is.na(main_agg)] <- 0
+  main_agg <- dummy_rows(main_agg, select_columns = c("ori", "time_unit"),
+                         dummy_value = NA)
+
+  if (!is.null(variables)) {
+    for (variable in variables) {
+      cols_temp <- c("ori",
+                     "time_unit",
+                     "offense",
+                     variable)
+      temp_agg <- data %>%
+        count_(cols_temp)
+
+      # Not all crimes can have weapons involved.
+      # Subsets to only ones that can
+      if (variable %in% "gun_involved") {
+        temp_agg <-
+          data %>%
+          count_(cols_temp) %>%
+          filter(offense %in% paste("offense", weapon_crimes))
+      }
+      # Only person victims ("individual" or "law enforcement officer"
+      # can have demographics)
+      if (victim_type) {
+        temp_agg <-
+          data %>%
+          filter(type_of_victim %in% person_victim_types) %>%
+          count_(cols_temp)
+      }
+      # Not all crimes can have victim injury involved.
+      # Subsets to only ones that can
+      if (variable %in% "victim_injury") {
+        temp_agg <-
+          temp_agg %>%
+          filter(offense %in% paste("victim", injury_crimes))
+      }
+
+
+      temp_agg$offense_variable <- paste(temp_agg[, variable], temp_agg$offense)
+      temp_agg[, variable] <- NULL
+      temp_agg <-
+        temp_agg %>%
+        select(-offense) %>%
+        pivot_wider(names_from  = offense_variable,
+                    values_from = n) %>%
+        rename_all(make_clean_names)
+      temp_agg[is.na(temp_agg)] <- 0
+      temp_agg <- dummy_rows(temp_agg, select_columns = c("ori", "time_unit"),
+                             dummy_value = NA)
+
+      main_agg <-
+        main_agg %>%
+        left_join(temp_agg, by = c("ori", "time_unit"))
+
+    }
+  }
+
+  names(main_agg) <- gsub("_offense_", "_", names(main_agg))
+  names(main_agg) <- gsub("_victim_", "_", names(main_agg))
+  names(main_agg) <- gsub("_arrestee_", "_", names(main_agg))
+  names(main_agg) <- gsub("_offender_", "_", names(main_agg))
+
+  return(main_agg)
+}
+
+
+
+make_largest_agency_json <- function(data) {
+  largest_agency <- data %>%
+    filter(year %in% max(data$year)) %>%
+    dplyr::group_by(state) %>%
+    dplyr::top_n(1, population) %>%
+    dplyr::select(state, agency)
+  largest_agency <- jsonlite::toJSON(largest_agency, pretty = TRUE)
+  write(largest_agency, "largest_agency_choices.json")
+}
+
+make_state_agency_choices <- function(data) {
+  data <- data.table::as.data.table(data)
+  for (selected_state in unique(data$state)) {
+    temp   <- data[state %in% selected_state]
+    agency <- unique(temp$agency)
+
+    agency <- jsonlite::toJSON(agency, pretty = FALSE)
+    write(agency, paste0(selected_state, "_agency_choices.json"))
+  }
+}
+
+make_agency_csvs <- function(data,
+                             type = "year") {
+
+  data <-
+    data %>%
+    dplyr::group_split(ORI)
+  parallel::mclapply(data,
+                     make_csv_test,
+                     type = type)
+
+}
+make_all_na <- function(col) {
+  col <- NA
+}
+
+make_csv_test <- function(temp, type) {
+
+  temp   <- dummy_rows_missing_years(temp, type = type)
+
+  state  <- unique(temp$state)
+  agency <- unique(temp$agency)
+  state  <- gsub(" ", "_", state)
+  agency <- gsub(" |:", "_", agency)
+  agency <- gsub("/", "_", agency)
+  agency <- gsub("_+", "_", agency)
+  agency <- gsub("\\(|\\)", "", agency)
+
+  data.table::fwrite(temp, file = paste0(state, "_", agency, ".csv"))
+}
+
+dummy_rows_missing_years <- function(data, type) {
+
+  if (type == "year") {
+    missing_years <- min(data$year):max(data$year)
+  } else {
+    missing_years <- seq.Date(lubridate::ymd(min(data$year)),
+                              lubridate::ymd(max(data$year)),
+                              by = "month")
+    missing_years <- as.character(missing_years)
+  }
+
+  missing_years <- missing_years[!missing_years %in% data$year]
+
+  if (length(missing_years) > 0) {
+
+    temp <- data
+    temp <- temp[1, ]
+    temp <- splitstackshape::expandRows(temp,
+                                        count = length(missing_years),
+                                        count.is.col = FALSE)
+    temp$year <- missing_years
+    temp <-
+      temp %>%
+      dplyr::mutate_at(vars(-one_of("year", "agency", "state", "ORI")),
+                       make_all_na)
+
+    data <-
+      data %>%
+      dplyr::bind_rows(temp) %>%
+      dplyr::arrange(desc(year)) %>%
+      dplyr::mutate(year = as.character(year))
+  }
+  return(data)
+}
+
+
